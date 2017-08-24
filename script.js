83bbae015ab6879e5378c66afee559e4d872ff2a
LuisFromDP <manzanero.luis1995@gmail.com>
2017-08-24 01:23:21 -0600

    Cleaned up more of the javascript.css and prepared the flow of the logic. Also made the html more responsive and added a reset button

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
   document.getElementById('weightInput').addEventListener('input', getWeightInput);
   function getWeightInput (e){
     document.getElementById('output').style.visibility = 'visible';
+    document.getElementById('resetBTN').style.visibility = 'visible';
    weight = e.target.value;//Gets input Value
    document.getElementById('weightOutput').innerHTML = weight;//placing the value to the DOM
    weightConverter();
@@ -29,11 +30,22 @@ document.getElementById('secondOption').addEventListener('change', getSecondOpti
 
 //Convert weights
 function weightConverter (){
-  document.getElementById('finalOutput').innerHTML = weight*5;
+  //Need to figure out the logic dynamically
+  if(firstOption == "Pounds" && secondOption == "Pounds"){
+    document.getElementById('finalOutput').innerHTML = weight*100;
+  } else if (firstOption == "Pounds" && secondOption == "Grams") {
+    document.getElementById('finalOutput').innerHTML = weight*1000;
+  } else if (firstOption == "Pounds" && secondOption == "Tons") {
+      document.getElementById('finalOutput').innerHTML = weight*10000;
+  } else {
+      document.getElementById('finalOutput').innerHTML = weight;
+  }
 }
 
 //Clears out all values and reset DOM
+  document.getElementById('resetBTN').addEventListener('click', reset);
   function reset(){
     document.getElementById('mainForm').reset();
     document.getElementById('output').style.visibility = 'hidden';
+    document.getElementById('resetBTN').style.visibility = 'hidden';
   }
